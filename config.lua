5e923c748d71466d49d1ad26cba176c8374569f8
GG79 <midlifegamingcrisis@gmail.com>
2024-01-14 10:56:07 -0800

    removed for fiveems

--- config.lua
+++ config.lua
@@ -344,6 +344,15 @@ Config.MenuItems = {
             }
         }
     },
+
+    [5] =  {
+        id = 'bed:up',
+        title = 'Stand Up',
+        icon = 'hand-point-up',
+        type = 'client',
+        event = 'bed:up',
+        shouldClose = true
+       },
 }
 
 Config.VehicleDoors = {
